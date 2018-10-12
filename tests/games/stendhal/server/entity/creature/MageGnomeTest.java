094cfd65187a538ac85d2277e91e374eea9fae05
Cheuk Nam Ryan Ho <cheuk.ho@student.manchester.ac.uk>
2018-10-12 12:34:04 +0100

    "Add comments, even monkey can understand my code"

--- tests/games/stendhal/server/entity/creature/MageGnomeTest.java
+++ tests/games/stendhal/server/entity/creature/MageGnomeTest.java
@@ -1,15 +1,5 @@
-/* $Id$ */
-/***************************************************************************
- *                   (C) Copyright 2003-2010 - Stendhal                    *
- ***************************************************************************
- ***************************************************************************
- *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
- *                                                                         *
- ***************************************************************************/
+// Written by Cheuk Nam Ryan Ho for testing the drop of mage gnome is potion not minor potion
+// with quantity 2-4 probability 40.0
 package games.stendhal.server.entity.creature;
 
 import static org.junit.Assert.assertTrue;
@@ -28,6 +18,7 @@ import utilities.RPClass.CreatureTestHelper;
 
 public class MageGnomeTest {
 
+	
 	@Before
 	public void setUp() {
 		MockStendlRPWorld.get();
@@ -40,15 +31,19 @@ public class MageGnomeTest {
 	public void tearDown() {
 		MockStendlRPWorld.reset();
 	}
-
+	
 	@Test
 	public void test() {;
-		Creature gnome = SingletonRepository.getEntityManager().getCreature("mage gnome");
-		assertTrue(gnome != null);
+		// Create Mage Gnome
+		Creature testgnome = SingletonRepository.getEntityManager().getCreature("mage gnome");
+		// to ensure a mage gnome is created, no more null pointer exception!!
+		assertTrue(testgnome != null);
 		boolean flag = false;
 		
-		for (DropItem item : gnome.dropsItems)
+		// check drop item
+		for (DropItem item : testgnome.dropsItems)
 		{
+			// for each item the mage gnome drops, if one of it is potion, than check its prob, min, max
 			if (item.name.equals("potion"))
 			{
 				flag = true;
@@ -56,16 +51,15 @@ public class MageGnomeTest {
 				assertTrue(item.min == 2);
 				assertTrue(item.max == 4);
 			}// if
+			// no more minor potion
 			if (item.name.equals("minor potion"))
 			{
 				flag = false;
 				
-			}
-		}
+			}// if
+		}// for
 		assertTrue(flag == true);
 		
-		//fail("Not yet implemented");
-		
-	}
+	}// test
 
-}
+}// class
