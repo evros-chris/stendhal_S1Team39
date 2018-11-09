0b38c7fc066575b3a5f48c1e2778c9dc6faac9cc
Petar Artinov <petar.artinov@student.manchester.ac.uk>
2018-11-09 11:34:49 +0000

    Removed straw carts bug fix so no tests fail.

--- src/games/stendhal/server/core/rp/StendhalRPAction.java
+++ src/games/stendhal/server/core/rp/StendhalRPAction.java
@@ -43,7 +43,6 @@ import games.stendhal.server.entity.creature.Pet;
 import games.stendhal.server.entity.creature.Sheep;
 import games.stendhal.server.entity.item.Item;
 import games.stendhal.server.entity.item.StackableItem;
-import games.stendhal.server.entity.mapstuff.block.Block;
 import games.stendhal.server.entity.npc.SpeakerNPC;
 import games.stendhal.server.entity.player.Player;
 import games.stendhal.server.events.AttackEvent;
@@ -556,19 +555,8 @@ public class StendhalRPAction {
 				return true;
 			}
 		}
-		//==================================================================================
-        //BLOCK PUSH FROM OUTSIDE OF ZONE BUG FIX
-		if (entity instanceof Player && zone.collides(entity, x, y))
-		{
-			List<Entity> entities = zone.getEntitiesAt(x, y);
-			for (Entity entit : entities)
-				if (entit instanceof Block)
-					((Block)entit).push((Player)entity, ((Player) entity).getDirection());
-		} // if
 		
-		else if (zone.collides(entity, x, y)) {
-	    //END OF BUG FIX
-	    //==================================================================================
+		if (zone.collides(entity, x, y)) {
 			boolean checkPath = true;
 			if (zone.collides(entity, x, y, false) && (entity instanceof Player)) {
 				// Trying to place a player on a spot with a real collision
