b58cd598f8f528709ad2bc40bac53a9a6fcf4895
Petar Artinov <petar.artinov@student.manchester.ac.uk>
2018-11-12 16:29:58 +0000

    Modified world set up and added player and scroll to the world.

--- tests/games/stendhal/server/entity/item/WofolCityScrollTest.java
+++ tests/games/stendhal/server/entity/item/WofolCityScrollTest.java
@@ -9,36 +9,42 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 import games.stendhal.server.core.engine.SingletonRepository;
+import games.stendhal.server.core.engine.StendhalRPWorld;
 import games.stendhal.server.core.engine.StendhalRPZone;
 import games.stendhal.server.entity.item.scroll.*;
 import games.stendhal.server.entity.player.Player;
 import games.stendhal.server.maps.MockStendlRPWorld;
 import marauroa.common.Log4J;
+import marauroa.common.game.RPObject.ID;
 import utilities.PlayerTestHelper;
 import utilities.RPClass.ItemTestHelper;
 
 public class WofolCityScrollTest {
+	
+	private static StendhalRPZone zone;
+	private static final String ZONE_NAME = "0_ados_forest_w2"; 
+	
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
 		Log4J.init();
-		MockStendlRPWorld.get();
+		final StendhalRPWorld world = MockStendlRPWorld.get();
 		ItemTestHelper.generateRPClasses();
-
-		MockStendlRPWorld.get().addRPZone(new StendhalRPZone("-1_semos_mine_nw"));
+		zone = new StendhalRPZone(ZONE_NAME);
+		world.addRPZone(zone);
 	}
 
 	@AfterClass
 	public static void tearDownAfterClass() throws Exception {
 		PlayerTestHelper.removeAllPlayers();
 	}
-
+		 
 	/**
 	 * Tests for description.
 	 */
 	@Test
 	public void testDescribe() {
 		final MarkedScroll scroll = (MarkedScroll) SingletonRepository.getEntityManager().getItem("wofol city scroll");
-		assertThat(scroll.describe(), is("You see a wofol city scroll, it will take you back to wofol city."));
+		assertThat(scroll.describe(), is("You see a §'wofol city scroll', it will take you back to wofol city. Upon it is written: -1_semos_mine_nw"));
 	} ///testDescribe
 
 	/**
@@ -47,8 +53,13 @@ public class WofolCityScrollTest {
 	@Test
 	public void testOnUsed() {
 		final Player bob = PlayerTestHelper.createPlayer("bob");
-		final MarkedScroll scroll = (MarkedScroll) SingletonRepository.getEntityManager().getItem("wofol city scroll");
+		PlayerTestHelper.registerPlayer(bob, zone);
+		
+		final TeleportScroll scroll = (TeleportScroll) SingletonRepository.getEntityManager().getItem("wofol city scroll");
+		scroll.setID(new ID(1, "wofol city scroll"));
+		zone.add(scroll);
         scroll.onUsed(bob);
+        
 
 		assertEquals(52, bob.getX());
         assertEquals(68, bob.getY());
